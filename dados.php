137ddcfd38e5f30f7678b9c56a6841b09cdbba81
Caio Vitor <caiovalente74@gmail.com>
2026-04-25 12:45:52 -0300

    criei a pagina de detalhes do pacote e fiz ajustes de layaut
    Criei o arquivo dados.php com um array associativo que armazena os dados de cada pacote, elaborei o arquivo detalhes.php onde adicionei um rascunho com os detalhes e a descrição e adicionei o formulário para o usuário contratar o pacote(Formulário ainda não funciona e não direciona para nenhuma página)

---
+++ dados.php
@@ -0,0 +1,39 @@
+<?php
+#array com os dados de cada pacote
+#futuramente irei adicionar uma descricao e imagens personalizadas para cada pacote 
+$pacotes = [
+  [
+    'id' => 1,
+    'nome' => 'Salvador',
+    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'valor' => 1200,
+    'imagem' => 'Salvador_img.jpg',
+    'disponivel' => 10
+  ],
+  [
+    'id' => 2,
+    'nome' => 'Rio de Janeiro',
+    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'valor' => 1500,
+    'imagem' => 'Rio_img.jpg',
+    'disponivel' => 5
+  ],
+  [
+    'id' => 3,
+    'nome' => 'Amazonas',
+    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'valor' => 1800,
+    'imagem' => 'Amazonas_img.jpg',
+    'disponivel' => 0
+  ],
+  [
+    'id' => 4,
+    'nome' => 'Gramado',
+    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'valor' => 2000,
+    'imagem' => 'Gramado_img.jpg',
+    'disponivel' => 8
+  ]
+];
+
+?>
