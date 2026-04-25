5dd14c11ab31c4f5b7655d9f5f4063dbd2c279f4
Caio Vitor <caiovalente74@gmail.com>
2026-04-24 23:03:26 -0300

    Realizei a navegação entre as páginas
    Adicionei o recurso de navegação entre as páginas, agora o botão "ver pacote" direciona para a pagina "detalhes.php" onde futuramente será possível ver os detalhes do pacote selecionado.

--- requires/footer.php
+++ requires/footer.php
@@ -1,78 +1,38 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<!--Footer que peguei pronto no bootstrap futuramente irei altera-lo e estiliza-lo-->
+<footer class="bg-dark text-white mt-5">
 
-<footer class="text-center bg-body-tertiary">
-  <!-- Grid container -->
-  <div class="container pt-4">
-    <!-- Section: Social media -->
-    <section class="mb-4">
-      <!-- Facebook -->
-      <a
-        data-mdb-ripple-init
-        class="btn btn-link btn-floating btn-lg text-body m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
+  <div class="container p-4">
+    <div class="row">
 
-      <!-- Twitter -->
-      <a
-        data-mdb-ripple-init
-        class="btn btn-link btn-floating btn-lg text-body m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-twitter"></i
-      ></a>
+      <!-- Sobre a nossa agencia ... -->
+      <div class="col-md-4">
+        <h5>Agência de Turismo</h5>
+        <p>Realizando viagens incríveis pelo Brasil com conforto e segurança</p>
+      </div>
 
-      <!-- Google -->
-      <a
-        data-mdb-ripple-init
-        class="btn btn-link btn-floating btn-lg text-body m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-google"></i
-      ></a>
+      <!-- Links do footer (padrão) ja veio junto apenas adaptei  -->
+      <div class="col-md-4">
+        <h5>Links</h5>
+        <ul class="list-unstyled">
+          <li><a href="../index.php" class="text-white text-decoration-none">Início</a></li>
+          <li><a href="#" class="text-white text-decoration-none">Destinos</a></li>
+          <li><a href="#" class="text-white text-decoration-none">Pacotes</a></li>
+          <li><a href="#" class="text-white text-decoration-none">Contato</a></li>
+        </ul>
+      </div>
 
-      <!-- Instagram -->
-      <a
-        data-mdb-ripple-init
-        class="btn btn-link btn-floating btn-lg text-body m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-instagram"></i
-      ></a>
+      <!-- Aba de contatos padrão de footer ,completar futuramente  -->
+      <div class="col-md-4">
+        <h5>Contato</h5>
+        <p>Email: EMAIL IMAGINARIO DA AGENCIA @GMAIL.COM</p>
+        <p>Telefone: (31) 00000000</p>
+      </div>
 
-      <!-- Linkedin -->
-      <a
-        data-mdb-ripple-init
-        class="btn btn-link btn-floating btn-lg text-body m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-linkedin"></i
-      ></a>
-      <!-- Github -->
-      <a
-        data-mdb-ripple-init
-        class="btn btn-link btn-floating btn-lg text-body m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-github"></i
-      ></a>
-    </section>
-    <!-- Section: Social media -->
+    </div>
   </div>
-  <!-- Grid container -->
 
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(20, 18, 18, 0.05);">
-    © 2020 Copyright:
-    <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  <div class="text-center p-3 bg-secondary">
+    © 2026 Agência de Turismo (por enquanto sem nome)
   </div>
-  <!-- Copyright -->
+
 </footer>
