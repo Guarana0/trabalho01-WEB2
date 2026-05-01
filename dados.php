85b4175aadaf6367b71fd63523165b9010c35cae
carlosafsv368 <carlos.afsv2008@gmail.com>
2026-05-01 18:38:03 -0300

    Dois pacotes foram adicionados, e a verificação das datas e a taxa por estadia longa foram acrescentadas

--- dados.php
+++ dados.php
@@ -5,7 +5,7 @@ $pacotes = array (
   array (
     'id' => 1,
     'nome' => 'Salvador',
-    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'descricao' => 'Mergulhe na essência baiana! O epicentro cultural brasileiro, que reverencia a herança afro-brasileira nas ladeiras do Centro Histórico, unindo a arquitetura colonial preservada. Explore também as águas da Baía de Todos-os-Santos, que banham a histórica Ribeira, até a orla oceânica do Rio Vermelho e Itapuã.',
     'valor' => 1200,
     'imagem' => 'Salvador_img.jpg',
     'disponivel' => 0,
@@ -19,7 +19,7 @@ $pacotes = array (
   array (
     'id' => 2,
     'nome' => 'Rio de Janeiro',
-    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'descricao' => 'Se aventure no lugar em que o mar e a montanha se encontram. Da vista monumental do Cristo Redentor às belas águas do Oceano Atlântico, bem como o histórico Pão de Açúcar.',
     'valor' => 1500,
     'imagem' => 'Rio_img.jpg',
     'disponivel' => 4,
@@ -33,7 +33,7 @@ $pacotes = array (
   array (
     'id' => 3,
     'nome' => 'Amazonas',
-    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'descricao' => 'Explore o grandioso ecossistema amazônico. Uma experiência voltada para a consciência, permitindo o contato com a fauna, a flora e os saberes das incríveis comunidades locais que guardam os segredos da maior floresta tropical do planeta.',
     'valor' => 1800,
     'imagem' => 'Amazonas_img.jpg',
     'disponivel' => 0,
@@ -43,11 +43,31 @@ $pacotes = array (
   array (
     'id' => 4,
     'nome' => 'Gramado',
-    'descricao' => 'Vou adicionar alguma descricao depois...',
+    'descricao' => 'A sofisticada Serra Gaúcha em um destino planejado para o conforto. Sendo referência em hospitalidade, Gramado une o clima de montanha a uma infraestrutura de altíssimo padrão e gastronomia influenciada pelos imigrantes europeus.',
     'valor' => 2000,
     'imagem' => 'Gramado_img.jpg',
     'disponivel' => 5,
     'categoria' => 'cidade',
+  ),
+  4 => 
+  array (
+    'id' => 5,
+    'nome' => 'Ouro Preto',
+    'descricao' => 'Um encontro profundo com o barroco e a história do ciclo do ouro. Ouro Preto oferece imersão na arte de Aleijadinho e nos episódios que moldaram a pátria verde e amarela, cercada pelos grandes morros mineiros.',
+    'valor' => 1600,
+    'imagem' => 'ouropreto_img.jpg',
+    'disponivel' => 7,
+    'categoria' => 'cidade',
+  ),
+  5 => 
+  array (
+    'id' => 6,
+    'nome' => 'Foz do Iguaçu',
+    'descricao' => 'Testemuhe um dos maiores espetáculos naturais da Terra. Um destino que fascina pela enorme força das águas e a vasta diversidade da Mata Atlântica, é ideal para quem busca contemplação e experiências sensoriais marcantes.',
+    'valor' => 2500,
+    'imagem' => 'fozdoiguacu_img.jpg',
+    'disponivel' => 15,
+    'categoria' => 'natureza',
   ),
 );
 ?>
