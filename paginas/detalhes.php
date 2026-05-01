34b23b102c287119d2b13a5a66319e6bcf5fdbad
Caio Vitor <caiovalente74@gmail.com>
2026-05-01 12:03:18 -0300

    Update detalhes.php

--- paginas/detalhes.php
+++ paginas/detalhes.php
@@ -2,10 +2,13 @@
 require '../dados.php';
 require '../requires/header.php';
 
+#id armazena o id do pacote recebido via GET
 $id = $_GET['id'] ?? null;
 
 $pacote = null;
 
+
+#loop para percorrer o array pacotes, ate achar o pacote como o id correspondente ao pacote desejado--> 
 foreach ($pacotes as $p) {
   if ($p['id'] == $id) {
     $pacote = $p;
@@ -25,13 +28,13 @@ if (!$pacote) {
 
   <div class="row">
 
-    <!-- IMAGEM -->
+    <!-- imagem de acordo com o pacote selecionado-->
     <div class="col-md-6">
       <img src="../imagens/<?= htmlspecialchars($pacote['imagem']) ?>" 
            class="img-fluid rounded">
     </div>
 
-    <!-- INFO -->
+    <!-- informacoes do pacote ,dinamica de acordo com o pacote selecionado-->
     <div class="col-md-6">
 
       <h2><?= htmlspecialchars($pacote['nome']) ?></h2>
@@ -44,18 +47,20 @@ if (!$pacote) {
 
       <!-- disponibilidade -->
       <?php if ($pacote['disponivel'] > 0): ?>
+        <!--usei o badge bg-sucess para o icone ficar verde caso esteja disponivel-->
         <span class="badge bg-success">Disponível</span>
       <?php else: ?>
+        <!--e o danger para ficar vermelho caso esteja esgotado-->
         <span class="badge bg-danger">Esgotado</span>
       <?php endif; ?>
 
       <hr>
 
-      <!-- FORM -->
+      <!-- Formulario que tera o seu resultadol exibido em resultado.php -->
       <form action="resultado.php" method="POST">
 
         <input type="hidden" name="id" value="<?= $pacote['id'] ?>">
-
+<!--todos os itens do foprmulario eu coloquei como required ja que e impossivel comtratar um pacote sem fornece-los-->
         <div class="mb-3">
           <label>Nome</label>
           <input type="text" name="nome" class="form-control" required>
@@ -65,7 +70,8 @@ if (!$pacote) {
           <label>Quantidade</label>
           <input type="number" name="quantidade" class="form-control" min="1" required>
         </div>
-
+<!--caso nao tenha pacote disponivel ou seja o numero de pacotes
+ diponiveis == 0 o botão de reservar fica desabilitado-->
         <button class="btn btn-success w-100"
           <?= $pacote['disponivel'] == 0 ? 'disabled' : '' ?>>
           Reservar
